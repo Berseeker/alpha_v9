b8d2e173b3db3e70871b0aba6d77b5e2e91f52b6
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-06-18 20:29:10 +0200

    deleteable categorias y subcategorias

--- app/Models/Subcategoria.php
+++ app/Models/Subcategoria.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 
 class Subcategoria extends Model
 {
-    use HasFactory,Searchable;
+    use HasFactory,Searchable,SoftDeletes;
 
     public function searchableAs()
     {
