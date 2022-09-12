c7f98ebad457f3e02884e82c669940a66aa09b6b
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-09-12 21:16:14 +0200

    header recaptcha missing

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -51,7 +51,7 @@ $configData = Helper::applClasses();
     <script src="{{asset('js/home/stack_motion/anime.min.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");</script>
-    
+    {!! htmlScriptTagJsApi(['lang' => 'es']) !!}
 
     @yield('vendor-style')
 
