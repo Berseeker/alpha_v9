c67fd4bf0d33bc9a82069a9b1ce969b68392db80
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2022-12-03 22:03:19 +0100

    debug promoopcion api

--- app/Http/Controllers/API/Proveedores/PromoOpcionController.php
+++ app/Http/Controllers/API/Proveedores/PromoOpcionController.php
@@ -28,6 +28,7 @@ class PromoOpcionController extends Controller
             ]);
         }
         $count = 0;
+        dd($result);
         foreach ($result as $key => $item) 
         {
             $prevItem = Producto::where('SDK',$item['parent_code'])->where('proveedor','PromoOpcion')->first();
