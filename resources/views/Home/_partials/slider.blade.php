0a2cd03f7a18678f4083559962eedd0beead8eb9
Juan Pablo Gomez <85317633+DevGPH@users.noreply.github.com>
2023-05-06 16:12:42 +0200

    dia del padre

--- resources/views/Home/_partials/slider.blade.php
+++ resources/views/Home/_partials/slider.blade.php
@@ -2,10 +2,10 @@
 
     <!-- Slides -->
     @foreach ($imagenes as $imagen)
-        @if ($imagen->seccion == 'home_slider')           
-            <a href="{{ ($imagen->pdf == null) ? '#' : Storage::url($imagen->pdf) }}" target="{{ ($imagen->pdf == null) ? '' : '_blank'}}" class="item"><img src="{{ Storage::url($imagen->path) }}" alt="" height="600px;" class="customImgSilder"></a>              
+        @if ($imagen->seccion == 'home_slider')
+            <a href="{{ ($imagen->pdf == null) ? '#' : Storage::url($imagen->pdf) }}" target="{{ ($imagen->pdf == null) ? '' : '_blank'}}" class="item"><img src="{{ Storage::url($imagen->path) }}" alt="" height="600px;" class="customImgSilder"></a>
         @endif
     @endforeach
-    <a href="https://heyzine.com/flip-book/5d45bf3aff.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/infancias.png') }}" class="w-100" alt="Dia del niño"> </a>
+    <a href="https://heyzine.com/flip-book/1fec8a191c.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/padre.png') }}" class="w-100" alt="Dia del Padre"> </a>
     <a href="https://heyzine.com/flip-book/3235a7e578.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/madres.png') }}" class="w-100" alt="Dia de las madres"> </a>
 </div>
