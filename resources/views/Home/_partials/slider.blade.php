acecaabb4d8b2e1685f9d06206a8e16d6baf50c4
BrainLabsDev <admin@brainlabs.tech>
2024-02-27 22:36:03 +0100

    8m slider

--- resources/views/Home/_partials/slider.blade.php
+++ resources/views/Home/_partials/slider.blade.php
@@ -6,6 +6,6 @@
             <a href="{{ ($imagen->pdf == null) ? '#' : Storage::url($imagen->pdf) }}" target="{{ ($imagen->pdf == null) ? '' : '_blank'}}" class="item"><img src="{{ Storage::url($imagen->path) }}" alt="" height="600px;" class="customImgSilder"></a>
         @endif
     @endforeach
-    <!--a href="https://heyzine.com/flip-book/97817b7fb9.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/bowl.jpeg') }}" class="w-100" alt="Super Bowl"> </a>
-    <a href="https://heyzine.com/flip-book/b3c8f4cce4.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/amor.jpeg') }}" class="w-100" alt="14 de Febrero"> </a-->
+    <a href="https://heyzine.com/flip-book/9543825ea8.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/march_women.jpg') }}" class="w-100" alt="8M"> </a>
+    <!--a href="https://heyzine.com/flip-book/b3c8f4cce4.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/amor.jpeg') }}" class="w-100" alt="14 de Febrero"> </a-->
 </div>
