83aeb1589a8d9f5948a4a738e76536d3dff4b977
BrainLabsDev <admin@brainlabs.tech>
2024-03-11 18:12:03 +0100

    new banners

--- resources/views/Home/_partials/slider.blade.php
+++ resources/views/Home/_partials/slider.blade.php
@@ -6,6 +6,6 @@
             <a href="{{ ($imagen->pdf == null) ? '#' : Storage::url($imagen->pdf) }}" target="{{ ($imagen->pdf == null) ? '' : '_blank'}}" class="item"><img src="{{ Storage::url($imagen->path) }}" alt="" height="600px;" class="customImgSilder"></a>
         @endif
     @endforeach
-    <a href="https://heyzine.com/flip-book/9543825ea8.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/march_women.jpg') }}" class="w-100" alt="8M"> </a>
-    <!--a href="https://heyzine.com/flip-book/b3c8f4cce4.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/amor.jpeg') }}" class="w-100" alt="14 de Febrero"> </a-->
+    <a href="https://heyzine.com/flip-book/63a56db7a2.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/day_kid.jpg') }}" class="w-100" alt="8M"> </a>
+    <a href="https://heyzine.com/flip-book/1bd795168a.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/vacations.jpg') }}" class="w-100" alt="14 de Febrero"> </a>
 </div>
