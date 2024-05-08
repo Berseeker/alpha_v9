128c1e56dfb0f484015d69e3ce44e28592a8afbb
BrainLabsDev <admin@brainlabs.tech>
2024-05-08 22:33:53 +0200

    new slider

--- resources/views/Home/_partials/slider.blade.php
+++ resources/views/Home/_partials/slider.blade.php
@@ -6,6 +6,5 @@
             <a href="{{ ($imagen->pdf == null) ? '#' : Storage::url($imagen->pdf) }}" target="{{ ($imagen->pdf == null) ? '' : '_blank'}}" class="item"><img src="{{ Storage::url($imagen->path) }}" alt="" height="600px;" class="customImgSilder"></a>
         @endif
     @endforeach
-    <a href="https://heyzine.com/flip-book/63a56db7a2.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/day_kid.jpg') }}" class="w-100" alt="8M"> </a>
-    <a href="https://heyzine.com/flip-book/1bd795168a.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/vacations.jpg') }}" class="w-100" alt="14 de Febrero"> </a>
+    <a href="https://heyzine.com/flip-book/09553f0e61.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/padre.jpeg') }}" class="w-100" alt="Dial del Padre"> </a>
 </div>
