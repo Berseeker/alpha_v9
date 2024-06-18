9d7517d88b2ba01f2a5cc5a4206919e209bc75af
BrainLabsDev <admin@brainlabs.tech>
2024-06-18 22:03:41 +0200

    new sliders

--- resources/views/Home/_partials/slider.blade.php
+++ resources/views/Home/_partials/slider.blade.php
@@ -6,5 +6,7 @@
             <a href="{{ ($imagen->pdf == null) ? '#' : Storage::url($imagen->pdf) }}" target="{{ ($imagen->pdf == null) ? '' : '_blank'}}" class="item"><img src="{{ Storage::url($imagen->path) }}" alt="" height="600px;" class="customImgSilder"></a>
         @endif
     @endforeach
-    <a href="https://heyzine.com/flip-book/09553f0e61.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/padre.jpeg') }}" class="w-100" alt="Dial del Padre"> </a>
+    <a href="https://heyzine.com/flip-book/2c7d81ef12.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/lluvia.jpg') }}" class="w-100" alt="Dias Lluvios"> </a>
+    <a href="https://heyzine.com/flip-book/0fe913e035.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/orgullo.jpg') }}" class="w-100" alt="Dial del Orgullo"> </a>
+    <a href="#" class="item"> <img src="{{ asset('imgs/v3/slider/general.jpg') }}" class="w-100" alt="AlphaPromos"> </a>
 </div>
