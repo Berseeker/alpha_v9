8c2f77013da8f40e19e415697f5ae3fd51e71626
BrainLabsDev <admin@brainlabs.tech>
2024-08-09 21:39:30 +0200

    new sliders web

--- resources/views/Home/_partials/slider.blade.php
+++ resources/views/Home/_partials/slider.blade.php
@@ -6,7 +6,7 @@
             <a href="{{ ($imagen->pdf == null) ? '#' : Storage::url($imagen->pdf) }}" target="{{ ($imagen->pdf == null) ? '' : '_blank'}}" class="item"><img src="{{ Storage::url($imagen->path) }}" alt="" height="600px;" class="customImgSilder"></a>
         @endif
     @endforeach
-    <a href="https://heyzine.com/flip-book/2c7d81ef12.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/lluvia.jpg') }}" class="w-100" alt="Dias Lluvios"> </a>
-    <a href="https://heyzine.com/flip-book/0fe913e035.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/orgullo.jpg') }}" class="w-100" alt="Dial del Orgullo"> </a>
+    <a href="https://heyzine.com/flip-book/c3c74ac4e9.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/agendas_24.svg') }}" class="w-100" alt="Agendas 2024"> </a>
+    <a href="https://heyzine.com/flip-book/d346a36865.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/clases_24.svg') }}" class="w-100" alt="Regreso a Clases"> </a>
     <a href="#" class="item"> <img src="{{ asset('imgs/v3/slider/general.jpg') }}" class="w-100" alt="AlphaPromos"> </a>
 </div>
