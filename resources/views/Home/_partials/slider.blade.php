3c6632174796653321120e01b58959f2faa61158
BrainLabsDev <admin@brainlabs.tech>
2024-08-12 22:17:34 +0200

    new slider

--- resources/views/Home/_partials/slider.blade.php
+++ resources/views/Home/_partials/slider.blade.php
@@ -7,6 +7,6 @@
         @endif
     @endforeach
     <a href="https://heyzine.com/flip-book/c3c74ac4e9.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/agendas_24.svg') }}" class="w-100" alt="Agendas 2024"> </a>
-    <!--a href="https://heyzine.com/flip-book/d346a36865.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/clases_24.svg') }}" class="w-100" alt="Regreso a Clases"> </a-->
+    <a href="https://heyzine.com/flip-book/090004d0bd.html" target="_blank" class="item"> <img src="{{ asset('imgs/v3/slider/clases_24.svg') }}" class="w-100" alt="Regreso a Clases"> </a>
     <a href="#" class="item"> <img src="{{ asset('imgs/v3/slider/general.jpg') }}" class="w-100" alt="AlphaPromos"> </a>
 </div>
